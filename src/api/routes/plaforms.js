2232b081c35d643ec755764c37390bc268373377
SirEduardo <eduardosaanchezlopez@gmail.com>
2024-06-25 12:32:43 +0200

    authorization

--- src/api/routes/plaforms.js
+++ src/api/routes/plaforms.js
@@ -1,11 +1,12 @@
 const upload = require("../../middlewares/file")
+const { isAuth } = require("../../middlewares/auth.middleware")
 const { getPlatform, postPlatform, deletePlatform, updatePlatform } = require("../controllers/platforms")
 
 const platformRoutes = require("express").Router()
 
 platformRoutes.get("/", getPlatform)
-platformRoutes.post("/", upload.single("img"), postPlatform)
-platformRoutes.delete("/:id", deletePlatform)
-platformRoutes.put("/:id", upload.single("img"), updatePlatform)
+platformRoutes.post("/", upload.single("img"), [isAuth], postPlatform)
+platformRoutes.delete("/:id", [isAuth], deletePlatform)
+platformRoutes.put("/:id", upload.single("img"), [isAuth], updatePlatform)
 
 module.exports = platformRoutes
--- src/api/routes/videogames.js
+++ src/api/routes/videogames.js
@@ -1,11 +1,12 @@
 const upload = require("../../middlewares/file")
+const { isAuth } = require("../../middlewares/auth.middleware")
 const { getVideogames, postVideogame, deleteVideogame, updateVideogame } = require("../controllers/videogames")
 
 const videogamesRoutes = require("express").Router()
 
 videogamesRoutes.get("/", getVideogames)
-videogamesRoutes.post("/", upload.single("img"), postVideogame)
-videogamesRoutes.delete("/:id", deleteVideogame)
-videogamesRoutes.put("/:id", upload.single("img"), updateVideogame)
+videogamesRoutes.post("/", upload.single("img"), [isAuth], postVideogame)
+videogamesRoutes.delete("/:id", [isAuth], deleteVideogame)
+videogamesRoutes.put("/:id", upload.single("img"), [isAuth], updateVideogame)
 
 module.exports =  videogamesRoutes 
